b6e7bbd84f506d3289154cd753fa7bc897edd8d2
piratjsk <piratjsk@gmail.com>
2016-06-12 18:47:43 +0200

    hide blocked commands from tab completions

--- src/main/java/net/piratjsk/nocmds/NoCmds.java
+++ src/main/java/net/piratjsk/nocmds/NoCmds.java
@@ -5,9 +5,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.event.server.TabCompleteEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.spigotmc.SpigotConfig;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class NoCmds extends JavaPlugin implements Listener {
@@ -21,6 +23,26 @@ public final class NoCmds extends JavaPlugin implements Listener {
 
         this.getServer().getPluginManager().registerEvents(this, this);
         this.getCommand("nocmds").setExecutor(this);
+    }
+
+    @Override
+    public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
+        if (sender.hasPermission("nocmds.admin")) {
+            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
+                this.reloadConfig();
+                this.blockedCommands = this.getConfig().getStringList("blockedCommands");
+                sender.sendMessage("[NoCmds] Configuration was reloaded.");
+                return true;
+            }
+            if (args.length == 1 && args[0].equalsIgnoreCase("list")) {
+                sender.sendMessage("[NoCmds] blockedCommands:");
+                for (String command : blockedCommands)
+                    sender.sendMessage(" "+command);
+                return true;
+            }
+        }
+        sender.sendMessage("[NoCmds] :)");
+        return true;
     }
 
     @EventHandler
@@ -48,24 +70,23 @@ public final class NoCmds extends JavaPlugin implements Listener {
         }
     }
 
-    @Override
-    public boolean onCommand(final CommandSender sender, final Command cmd, final String label, final String[] args) {
-        if (sender.hasPermission("nocmds.admin")) {
-            if (args.length == 1 && args[0].equalsIgnoreCase("reload")) {
-                this.reloadConfig();
-                this.blockedCommands = this.getConfig().getStringList("blockedCommands");
-                sender.sendMessage("[NoCmds] Configuration was reloaded.");
-                return true;
-            }
-            if (args.length == 1 && args[0].equalsIgnoreCase("list")) {
-                sender.sendMessage("[NoCmds] blockedCommands:");
-                for (String command : blockedCommands)
-                    sender.sendMessage(" "+command);
-                return true;
+    @EventHandler
+    public void onCommandTabComplete(final TabCompleteEvent event) {
+        if (event.getSender().hasPermission("nocmds.bypass")) return;
+        final ArrayList<String> completions = new ArrayList<String>(event.getCompletions());
+        for (final String cmplt : (ArrayList<String>)completions.clone()) {
+            for (final String blockedCmd : this.blockedCommands) {
+                if (blockedCmd.startsWith("/")) {
+                    if (cmplt.equalsIgnoreCase(blockedCmd)) completions.remove(cmplt);
+                } else {
+                    if (cmplt.contains(":") && cmplt.split(":")[1].equalsIgnoreCase(blockedCmd))
+                        completions.remove(cmplt);
+                    else if (cmplt.equalsIgnoreCase("/" + blockedCmd))
+                        completions.remove(cmplt);
+                }
             }
         }
-        sender.sendMessage("[NoCmds] :)");
-        return true;
+        event.setCompletions(completions);
     }
 
 }
